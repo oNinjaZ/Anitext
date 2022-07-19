6efe55cd5b5f94537dec3bf0b18558ac620de402
oNinjaZ <ninjaz.gg@outlook.com>
2022-07-19 15:25:20 -0400

    add db context and interfaces to services

--- src/Anitext.Api/Program.cs
+++ src/Anitext.Api/Program.cs
@@ -1,9 +1,19 @@
+using Anitext.Api.Data;
+using Anitext.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opts =>
+    opts.UseNpgsql(builder.Configuration.GetValue<string>("Database:ConnectionString")))
+    .AddSingleton<IQuoteRepository, QuoteRepository>()
+    .AddSingleton<ICharacterRepository, CharacterRepository>()
+    .AddSingleton<IAliasRepository, AliasRepository>()
+    .AddSingleton<IAnimeRepository, AnimeRepository>();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
